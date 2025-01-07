07f02e268bdf1cae95199e676be4b646a5adf253
rcsdk <rcsdk@proton.me>
2025-01-07 18:03:38 +0000

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,3 +1,312 @@
+IDEAL SCRIPT
+
+
+#Missing bash so we can edit 
+
+
+
+#Done - good like this, already tested (unless you know a better way)
+echo "bind \"^C\": copy" >> ~/.inputrc
+echo "bind \"^V\": paste" >> ~/.inputrc
+echo "bind \"^Z\": suspend" >> ~/.inputrc
+
+xfce4-terminal &
+
+
+#Done - good like this, already tested (unless you know a better way)
+xrandr -q
+alias br='xrandr --output eDP1 --brightness'
+br 0.4
+
+
+#Done - good like this, already tested (unless you know a better way)
+synclient TouchpadOff=1
+
+
+#Done - good like this, already tested (unless you know a better way)
+sudo rm -f /var/lib/pacman/db.lck
+
+
+#Still to be done
+install a browser - chrome or one that deals well with webgl so i can work on figma -
+open 5 tabs - with these urls already opened and logged in
+a nethod for me to add remove tabs in the future - also logged user pass
+
+
+#Still to be done
+open firefox in private mode, and check all the variables on that advanced part to see if its hardened as much as possible (several fragile points there)
+
+
+#Still to be done
+fix audio certify that mic is working - install dictation - test - 
+
+
+#I need to edit a tempered pacman.conf to be the best possible one adding mirrors including darker ones for pentesting, etc
+nano /etc/pacman.conf    (clean below)
+
+
+#Done - good like this, already tested (unless you know a better way)
+pacman-key --init
+gpg --check-trustdb
+pacman --Syy
+pacman -Syu
+pacman -Sy
+pacman -S --noconfirm linux
+pacman -S --noconfirm linux-firmware
+pacman -S --noconfirm linux-lts
+pacman -S linux-firmware
+sudo mkinitcpio -p linux
+sudo pacman -Syu
+lsmod | grep xhci_pci
+lsmod | grep ast
+lsmod | grep aic94xx
+lsmod | grep wd719x
+dmesg | grep -i firmware
+dmesg | grep -i xhci_pci
+pacman -S --noconfirm volatility
+pacman -S --noconfirm memdump
+pacman -S --noconfirm lios
+pacman -S --noconfirm crash
+pacman -S --noconfirm metasploit
+pacman -S --noconfirm dirty_cow
+pacman -S --noconfirm stage
+pacman -S --noconfirm gdisk
+pacman -S --noconfirm grub
+pacman -S --noconfirm mkinitcpio
+pacman -S pulseaudio
+
+#Can be improved
+sudo pacman -Syu --noconfirm && sudo pacman -Syy --noconfirm
+sudo pacman -S --noconfirm \
+    thunar gvfs ntfs-3g udisks2 thunar-volman gvfs-mtp gvfs-smb
+sudo systemctl enable --now udisks2.service
+echo "Installed packages:"
+pacman -Q thunar gvfs ntfs-3g udisks2 thunar-volman gvfs-mtp gvfs-smb
+echo "udisks2 service status:"
+systemctl status udisks2.service --no-pager
+
+
+#Please add more, if you know
+sudo systemctl disable alsa-restore.service
+sudo systemctl disable getty@tty1.service
+sudo systemctl disable ip6tables.service
+sudo systemctl disable iptables.service
+sudo systemctl disable cups
+sudo systemctl disable avahi-daemon
+sudo systemctl disable bluetooth
+sudo systemctl mask alsa-restore.service
+sudo systemctl mask getty@tty1.service
+sudo systemctl mask ip6tables.service
+sudo systemctl mask iptables.service
+sudo systemctl mask cups
+sudo systemctl mask avahi-daemon
+sudo systemctl mask bluetooth
+
+
+#Can be improved Im sure:
+sudo systemd-analyze blame
+
+
+#Please add more, if you know  Can be improved
+sudo echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+
+
+# Disable unnecessary overlay features Can be improved - Malware do a LOT of overlay - on apps, on site, etc - whatever we can add to avoid it, the better
+sudo sed -i 's/ overlay//g' /etc/X11/xorg.conf
+
+
+# Disable unnecessary overlay features Can be improved - Malware do a LOT of overlay - on apps, on site, etc - whatever we can add to avoid it, the better
+sudo sed -i 's/ allow-overlay//g' /etc/security/limits.conf
+
+
+#Can be improved Im sure: Use a secure overlay network
+sudo pacman -S --noconfirm openvpn
+sudo systemctl enable openvpn
+
+#I dont want to add all of them on this script because we can add on the second one. This first is supposed to run fast so I Can start to work. Then I run the second. Install basic security tools
+sudo pacman -S --noconfirm ufw apparmor
+
+#Can be improved Im sure: Set up a firewall
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw reload
+
+
+#Can be improved Im sure: nable AppArmor
+sudo systemctl enable apparmor
+sudo systemctl start apparmor
+sudo aa-enforce /etc/apparmor.d/*
+
+#Can be improved Im sure:Install yay if not already installed
+if ! command -v yay &> /dev/null; then
+  sudo pacman -S --needed git base-devel --noconfirm
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay
+fi
+
+#Can be improved Im sure:Install whisper using yay
+yay -S whisper --noconfirm
+
+#Can be improved Im sure:Update and patch the system
+sudo pacman -Syu --noconfirm
+
+#Can be improved Im sure:Patch and harden the kernel
+sudo pacman -S --noconfirm linux-hardened linux-hardened-headers
+
+
+
+
+
+
+# Add Google Chrome repository to Pacman
+echo "[google-chrome]" | sudo tee /etc/pacman.conf
+echo "Server = https://dl.google.com/linux/chrome/$arch" | sudo tee -a /etc/pacman.conf
+sudo pacman-key --recv-keys --keyserver keyserver.ubuntu.com BA88F2723BA7FF56
+sudo pacman-key --lsign-key BA88F2723BA7FF56
+
+# Update and upgrade the system
+sudo pacman -Syu --noconfirm
+
+# Install Google Chrome
+sudo pacman -Syu google-chrome-stable --noconfirm
+
+# Install chrome-cli
+sudo pacman -S git --noconfirm
+git clone https://github.com/prasmussen/chrome-cli.git
+cd chrome-cli
+sudo make
+sudo cp chrome-cli /usr/local/bin/
+cd ..
+rm -rf chrome-cli
+
+# Open specified URLs in Google Chrome
+if command -v google-chrome &> /dev/null; then
+  google-chrome "https://account.proton.me/login" &
+  google-chrome "https://www.figma.com/login?locale=en-us" &
+  google-chrome "https://auth.openai.com/authorize?audience=https%3A%2F%2Fapi.openai.com%2Fv1&client_id=TdJIcbe16WoTHtN95nyywh5E4yOo6ItG&country_code=BR&device_id=6fe36ba7-fec1-41db-9716-dd645aad1492&ext-oai-did=6fe36ba7-fec1-41db-9716-dd645aad1492&prompt=login&redirect_uri=https%3A%2F%2Fchatgpt.com%2Fapi%2Fauth%2Fcallback%2Fopenai&response_type=code&scope=openid+email+profile+offline_access+model.request+model.read+organization.read+organization.write&screen_hint=login&state=dOWxn_4hanMG8XzgAjY2fMQNW9INMGwBjujroshuVT0&flow=treatment" &
+  google-chrome "https://venice.ai/chat/_xT7DNF0_-uaVdA-FVihq" &
+  google-chrome "https://www.freepik.com/log-in?client_id=freepik&lang=en" &
+  google-chrome "https://github.com/login?return_to=https%3A%2F%2Fgithub.com%2Fsignup%3Fnux_signup%3Dtrue" &
+fi
+
+# Install the Voice in Speech-to-Text Chrome extension
+chrome-cli install pjnefijmagpdjfhhkpljicbbpicelgko
+
+
+
+
+# Install additional security tools
+sudo pacman -S --noconfirm clamav rkhunter
+
+# Scan for malware and rootkits
+sudo freshclam
+sudo rkhunter --propupd
+sudo clamscan -r /
+sudo rkhunter --checkall
+
+
+
+
+
+
+
+
+
+EDIT PACMAN: this is the clean
+nano /etc/pacman.conf
+
+
+
+# NOTE: You must run `pacman-key --init` before first using pacman; the local
+# keyring can then be populated with the keys of all official Arch Linux
+# packagers with `pacman-key --populate archlinux`.
+
+[options]
+HoldPkg     = pacman glibc
+SyncFirst   = pacman
+Architecture = auto
+
+[core]
+SigLevel    = Required DatabaseOptional
+Include     = /etc/pacman.d/mirrorlist
+
+[extra]
+SigLevel    = Required DatabaseOptional
+Include     = /etc/pacman.d/mirrorlist
+
+[community]
+SigLevel    = Required DatabaseOptional
+Include     = /etc/pacman.d/mirrorlist
+
+[multilib]
+SigLevel    = Required DatabaseOptional
+Include     = /etc/pacman.d/mirrorlist
+
+[archlinuxfr]
+SigLevel    = Optional TrustAll
+Server      = http://repo.archlinux.fr/$arch
+
+[herecura]
+SigLevel    = Optional TrustAll
+Server      = http://repo.herecura.be/herecura-stable/$arch
+
+[antergos]
+SigLevel    = Optional TrustAll
+Server      = http://repo.antergos.com/$arch/$repo
+
+#[google-chrome]
+#SigLevel    = Optional TrustAll
+#Server      = http://dl.google.com/linux/chrome/rpm/stable/$arch
+
+#[testing]
+#SigLevel    = Optional TrustAll
+#Include     = /etc/pacman.d/mirrorlist
+
+#[community-testing]
+#SigLevel    = Optional TrustAll
+#Include     = /etc/pacman.d/mirrorlist
+
+#[multilib-testing]
+#SigLevel    = Optional TrustAll
+#Include     = /etc/pacman.d/mirrorlist
+
+#[kde-unstable]
+#SigLevel    = Optional TrustAll
+#Include     = /etc/pacman.d/mirrorlist
+
+#[gnome-unstable]
+#SigLevel    = Optional TrustAll
+#Include     = /etc/pacman.d/mirrorlist
+
+#[erikreider]
+#SigLevel    = Optional TrustAll
+#Server      = http://pkgbuild.com/~erikreider/repo/$arch
+
+#[haskell]
+#SigLevel    = Optional TrustAll
+#Server      = http://archhaskell.mynerdside.com/$repo/$arch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
 
 # Disable laptop trackpad
