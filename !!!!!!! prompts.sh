8249e12a6174f6ec7c9f66acc721c562bc2f36d4
RobCarloz! <rcsdk@proton.me>
2025-02-02 22:31:40 -0300

    Create !!!!!!! prompts.sh

---
+++ !!!!!!! prompts.sh
@@ -0,0 +1,11 @@
+code in bash to help me harden my envinroment - im on ram - 16g - booting on system rescue - 4, 5 times a day   booting from a strick to try and work as a designer.
+i want to develop 04 scripts. 
+01 - preparation offline
+02 - download and check all around for breaches - hidden services, hidden connections, opened ports (close all minus http), hidden ssh, kernel, change/refresh literally all confs (and similar) once he tempers all of them including clamav, rk chk etc etc
+03 - download and setup all security - firewall, etc etc etc - centralizing all logs - in a single place - minimum files as possible - condensing and creating files based on interpretation of really matters (I always feel there is 90% garbage)
+04 - check all the above again - to be sure they are working and will continue to work once malware coming from vram - can temper whatever he wants - creating a lot of confusion - eg. didnt we fix DNS already?? (and the same for EVERYTHING - so its very confusing...)
+
+
+This is the first - not yet finished. please give your opinion on the overall and improvements suggestions
+
+
